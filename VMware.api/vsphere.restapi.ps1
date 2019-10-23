1bea8d0e523f59953e6ab0eabb43e2e8151b9bb6
matalus <matt.hamende@gmail.com>
2019-10-22 19:14:06 -0700

    add VMware api

---
+++ VMware.api/vsphere.restapi.ps1
@@ -0,0 +1,61 @@
+#Script for interfacing with vCenter API - requires PS Core 6.x and up
+
+if($PSVersionTable.PSVersion.Major -lt 6){
+   Write-Error "This script requires PS Core 6.x and up"
+}
+
+$vcenter = "vcenter.uhaul.amerco.org"
+if(!$cred){
+   $cred = Get-Credential
+}
+
+#credentials for vcenter
+$apiuser = $cred.UserName
+$apipass = $cred.GetNetworkCredential().Password
+
+#Auth URL
+$BaseAuthURL = "https://$vcenter/rest/com/vmware/cis/"
+$BaseURL = "https://$vcenter/rest/vcenter/"
+$vCenterSessionURL = $BaseAuthURL + "session"
+$Header = @{"Authorization" = "Basic " +
+[System.Convert]::ToBase64String(
+   [System.Text.Encoding]::UTF8.GetBytes(
+      $apiuser + 
+      ":" +
+      $apipass
+   )
+)}
+
+$SessionParams = @{
+   Uri = "https://$vcenter/rest/com/vmware/cis/session"
+   Method = "POST"
+   Headers = $Header
+   ContentType = "application/json"
+   SkipCertificateCheck = $true
+}
+$SessionResponse = Invoke-RestMethod @SessionParams
+if($SessionResponse){
+   "vcenter-api-session-id: $($SessionResponse.value)"
+}
+
+$SessionHeader = @{'vmware-api-session-id' = $SessionResponse.value}
+
+$VMListURL = $BaseURL + "resource-pool"
+
+Try{
+   $RestParams = @{
+      Method = "GET"
+      URI = $VMListURL
+      TimeoutSec = 100
+      Headers = $SessionHeader
+      ContentType = 'application/json'
+      SkipCertificateCheck = $true
+   }
+   $ResponseJSON = Invoke-RestMethod @RestParams
+   "Result:"
+   $ResponseJSON.value
+}Catch{
+   $_.Exception.ToString()
+   $error[0] | Format-List -Force
+}
+
